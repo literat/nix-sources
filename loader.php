5cb5451966d26e2fcabf8a3514c85f6d98c3e45d
literat <tomaslitera@hotmail.com>
2014-09-11 11:42:45 +0200

    Nix framework loader
      * require needed files (not used in class files any more)
      * remove frameworkUrl function (part of application call)

--- loader.php
+++ loader.php
@@ -19,38 +19,10 @@
 
 ob_start();
 $startTime = microtime(true);
+
+require_once dirname(__FILE__) . '/common/Object.php';
+require_once dirname(__FILE__) . '/Loaders/AutoLoader.php';
 require_once dirname(__FILE__) . '/Loaders/NixLoader.php';
 
 $NixLoader = new Nix\Loaders\NixLoader();
 $NixLoader->register();
-
-/**
- * Processes the framework url
- *
- * @param string $url url
- * @param array $args rewrite args
- * @param array|false $params rewrite params
- * @return string
- */
-function frameworkUrl($url, $args = array(), $params = false) {
-	if(empty($url)) {
-		$url = Http::$request->request;
-	} else {
-		$url = preg_replace('#\<\:([a-z0-9]+)\>#ie', "isset(\$args['\1']) ? \$args['\1'] : ''", $url);
-	}
-
-	if($params !== false) {
-		$p = array();
-		$params = array_merge($_GET, (array) $params);
-		foreach($params as $name => $value) {
-			if ($value == null) continue;
-			$p[] = urlencode($name) . '=' . urlencode($value);
-		}
-
-		if(!empty($p)) {
-			$url .= '?' . implode('&', $p);
-		}
-	}
-
-	return Http::$baseURL . '/' . ltrim($url, '/');
-}
