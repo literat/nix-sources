264723d9fd454e6bb1b2a4d5d1f167b831350b37
literat <tomaslitera@hotmail.com>
2014-09-14 18:31:22 +0200

    Datepicker Control

---
+++ Nix/Forms/Controls/Datepicker.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of the Nix Framework
+ *
+ * Copyright (c) 2014 Tomáš Litera
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
+namespace Nix\Forms\Controls;
+
+use Nix\Forms\Controls\Input;
+
+/**
+ * Datepicker Control
+ *
+ * @author      Tomas Litera 	<tomaslitera@hotmail.com>
+ * @package     Nix
+ * @subpackage  Forms\Controls
+ */
+class Datepicker extends Input
+{
+	/**
+	 * Set the control value
+	 * 
+	 * @param mixed $value new value
+	 * @return bool
+	 */
+	public function setValue($value)
+	{
+		if(!empty($value)) {
+			$value = date('Y-m-d', strtotime($value));
+		}
+
+		parent::setValue($value);
+	}
+
+	/**
+	 * Returns value for html tag
+	 * 
+	 * @return string
+	 */
+	public function getHtmlValue()
+	{
+		$value = parent::getHtmlValue();
+		$value = preg_replace('#([0-9]{4})-([0-9]{2})-([0-9]{2})#', '$3.$2.$1', $value);
+		
+		return $value;		
+	}
+
+	/**
+	 * Returns Html object of form control
+	 * 
+	 * @return Html
+	 */
+	protected function getHtmlControl()
+	{
+		$control = parent::getHtmlControl();
+	
+		return $control->class('calendar');
+	}
+}
